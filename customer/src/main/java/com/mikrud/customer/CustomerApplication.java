64fa8fa4c2b4db8fb66ec6ad218f140177cf49cd
misharyduk <misharyduk64@gmail.com>
2023-01-23 17:05:51 +0200

    added api for registering customer

--- customer/src/main/java/com/mikrud/customer/CustomerApplication.java
+++ customer/src/main/java/com/mikrud/customer/CustomerApplication.java
@@ -2,10 +2,15 @@ package com.mikrud.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class CustomerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
+
     }
 }
---
+++ customer/src/main/java/com/mikrud/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.mikrud.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
+        // todo: check if email valid
+        // todo: check if email not taken
+        customerRepository.save(customer);
+    }
+}

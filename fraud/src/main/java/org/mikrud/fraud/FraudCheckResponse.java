57d7d99d3a87b557953b059dddb6c46736bc10fd
misharyduk <misharyduk64@gmail.com>
2023-01-27 16:21:00 +0200

    added fraud microservice

---
+++ fraud/src/main/java/org/mikrud/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package org.mikrud.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
---
+++ fraud/src/main/java/org/mikrud/fraud/FraudController.java
@@ -0,0 +1,19 @@
+package org.mikrud.fraud;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/fraud-check")
+@AllArgsConstructor
+public class FraudController {
+
+    private final FraudCheckService service;
+
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
+        boolean isFraudulentCustomer = service.isFraudulentCustomer(customerId);
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
+}
